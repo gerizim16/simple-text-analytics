e8511ea98a0febddccede2f30de3e94adee4734b
gerizim16 <gerizim16@gmail.com>
2020-07-19 17:51:45 +0800

    Added english stopwords removal

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import sys
 import collections
-import re
+import nltk
 
 
 class TextFile(object):
     def __init__(self, filename):
-        with open(filename, 'r') as text_file:
-            self._text = text_file.read().lower()
+        with open(filename, 'r', encoding='utf-8') as text_file:
+            self._text = text_file.read()
         self._word_counter = None
 
     def getWordFreq(self, n=None):
         if self._word_counter is None:
-            words = re.findall(r'\w+', self._text)
+            words = self.wordTokenize()
             self._word_counter = collections.Counter(words)
         return self._word_counter.most_common(n)
 
+    def wordTokenize(self):
+        tokens = nltk.word_tokenize(self._text)
+        stop_words = set(nltk.corpus.stopwords.words('english'))
+
+        words = map(str.lower, tokens)
+        return list(word for word in words if word not in stop_words)
+
+    def sentTokenize(self):
+        return nltk.sent_tokenize(self._text)
+
 
 def printMenu():
+    print('==========================')
     print('0, Exit')
     print('1. Get word frequency')
     print('2. Get most common N words')
+    print()
 
 
 if __name__ == '__main__':
@@ -32,12 +44,16 @@ if __name__ == '__main__':
     while True:
         printMenu()
         usr_input = input()
+
         if usr_input == '0':
             break
+        
         elif usr_input == '1':
-            with open('word_frequency.csv', 'w') as word_frequency_file:
+            with open('word_frequency.csv', 'w',
+                      encoding='utf-8') as word_frequency_file:
                 for data in text_file.getWordFreq():
                     word_frequency_file.write('{},{}\n'.format(*data))
+        
         elif usr_input == '2':
             n = int(input('Enter number of words: '))
             for data in text_file.getWordFreq(n):
